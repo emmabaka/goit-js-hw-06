bc31744ea6a490815c7daf7d66bc1f8aaa8cec51
emmabaka <bakaemma88@gmail.com>
2023-02-07 21:14:21 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,18 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
-];
+]; 
+
+
+const ingredientsList = document.querySelector("#ingredients");
+const itemArr = [];
+
+ingredients.forEach(element => {
+  const item = document.createElement("li");
+
+  item.textContent = element;
+  item.classList.add("item");
+  itemArr.push(item);
+});
+
+ingredientsList.append(...itemArr);
