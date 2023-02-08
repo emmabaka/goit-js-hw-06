110e2c016099f571a0c600568f65518b92c40c02
emmabaka <bakaemma88@gmail.com>
2023-02-08 14:12:05 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const currentColorName = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  currentColorName.textContent = document.body.style.backgroundColor;
+})
