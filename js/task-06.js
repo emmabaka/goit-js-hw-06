f2641bad64012f8ebf45f14efaf254aec67acaa9
emmabaka <bakaemma88@gmail.com>
2023-02-08 17:19:21 +0200

    corrected

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,12 @@
- const inputEl = document.querySelector("#validation-input");
- const validNumber = inputEl.dataset.length;
- 
- inputEl.addEventListener("blur", (e) => {
-    if (inputEl.value.length >= validNumber) {
-        inputEl.classList.add("valid")
-    } else {
-        inputEl.classList.add("invalid")
-    }
- })
+const inputEl = document.querySelector("#validation-input");
+const validNumber = inputEl.dataset.length;
+console.log(validNumber);
+
+inputEl.addEventListener("blur", () => {
+  console.log(inputEl.value.length);
+  if (inputEl.value.length === Number(validNumber)) {
+    inputEl.classList.add("valid");
+  } else {
+    inputEl.classList.add("invalid");
+  }
+});
