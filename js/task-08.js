aa2873c21147365e1670b0fb1e549d8d54e0bf94
emmabaka <bakaemma88@gmail.com>
2023-02-09 12:18:44 +0200

    corrected task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,19 @@
 const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", (e) => {
-e.preventDefault();
+  e.preventDefault();
 
-const formElements = e.currentTarget.elements;
+  const formElements = e.currentTarget.elements;
 
-if (formElements.email.value === "" || formElements.password.value === "") {
-    alert("Заповніть, будь ласка, всі поля форми;)")
-} else {
-    console.log(`email: ${formElements.email.value}`);
-    console.log(`password: ${formElements.password.value}`);
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    alert("Заповніть, будь ласка, всі поля форми;)");
+  } else {
+    const loginValuesObj = {
+      email: formElements.email.value,
+      password: formElements.password.value,
+    };
+    console.log(loginValuesObj);
+    
     e.currentTarget.reset();
-}
+  }
 });
