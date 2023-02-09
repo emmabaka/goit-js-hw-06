d78153d9e6a8ce85c8f01b922fddab67bddf5fb1
emmabaka <bakaemma88@gmail.com>
2023-02-09 12:13:16 +0200

    corrected task 6

--- js/task-06.js
+++ js/task-06.js
@@ -6,7 +6,9 @@ inputEl.addEventListener("blur", () => {
   console.log(inputEl.value.length);
   if (inputEl.value.length === Number(validNumber)) {
     inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
   } else {
     inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
   }
 });
